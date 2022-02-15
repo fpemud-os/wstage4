f6087e92fbf6fbe1f9cf06728a015151dd3fd1b2
fpemud <fpemud@163.com>
2022-02-15 08:02:11 +0000

    fix

--- python3/wstage4/_const.py
+++ python3/wstage4/_const.py
@@ -25,78 +25,80 @@ import enum
 
 
 class Arch(enum.Enum):
-    X86 = "X86"
-    X86_64 = "X86_64"
+    X86 = enum.auto()
+    X86_64 = enum.auto()
 
 
 class Category(enum.Enum):
-    WINDOWS_95 = "windows-95"
-    WINDOWS_98 = "windows-98"
-    WINDOWS_XP = "windows-xp"
-    WINDOWS_VISTA = "windows-vista"
-    WINDOWS_7 = "windows-7"
-    WINDOWS_8 = "windows-8"
-    WINDOWS_8_1 = "windows-8"
-    WINDOWS_10 = "windows-10"
-    WINDOWS_11 = "windows-11"
+    WINDOWS_95 = enum.auto()
+    WINDOWS_98 = enum.auto()
+    WINDOWS_XP = enum.auto()
+    WINDOWS_VISTA = enum.auto()
+    WINDOWS_7 = enum.auto()
+    WINDOWS_8 = enum.auto()
+    WINDOWS_8_1 = enum.auto()
+    WINDOWS_10 = enum.auto()
+    WINDOWS_11 = enum.auto()
 
-    WINDOWS_SERVER_2008 = "windows-server-2008"
-    WINDOWS_SERVER_2012 = "windows-server-2012"
-    WINDOWS_SERVER_2016 = "windows-server-2016"
-    WINDOWS_SERVER_2019 = "windows-server-2019"
+    WINDOWS_SERVER_2008 = enum.auto()
+    WINDOWS_SERVER_2012 = enum.auto()
+    WINDOWS_SERVER_2016 = enum.auto()
+    WINDOWS_SERVER_2019 = enum.auto()
 
 
 class Edition(enum.Enum):
-    WINDOWS_95 = "windows-95"
+    # from https://en.wikipedia.org/wiki/Microsoft_Windows_version_history
+    WINDOWS_95 = enum.auto()                   # Windows 95
 
-    WINDOWS_98 = "windows-98"
-    WINDOWS_98_SE = "windows-98-se"
+    # from https://en.wikipedia.org/wiki/Microsoft_Windows_version_history
+    WINDOWS_98 = enum.auto()                   # Windows 98
+    WINDOWS_98_SE = enum.auto()                # Windows 98 SE
 
-    WINDOWS_XP_HOME = "windows-xp-home"
-    WINDOWS_XP_PROFESSIONAL = "windows-xp-professional"
+    WINDOWS_XP_HOME = enum.auto()
+    WINDOWS_XP_PROFESSIONAL = enum.auto()
 
-    WINDOWS_VISTA = "windows-vista"
-
-    # from https://www.windowsafg.com
-    WINDOWS_7_STARTER = "windows-7-starter"                 # Windows 7 Starter
-    WINDOWS_7_HOME_BASIC = "windows-7-home-basic"           # Windows 7 Home Basic
-    WINDOWS_7_HOME_PREMIUM = "windows-7-home-premium"       # Windows 7 Home Premium
-    WINDOWS_7_PROFESSIONAL = "windows-7-professional"       # Windows 7 Professional
-    WINDOWS_7_ULTIMATE = "windows-7-ultimate"               # Windows 7 Ultimate
-    WINDOWS_7_ENTERPRISE = "windows-7-enterprise"           # Windows 7 Enterprise
+    WINDOWS_VISTA = enum.auto()
 
     # from https://www.windowsafg.com
-    WINDOWS_8 = "windows-8"                                 # Windows 8
-    WINDOWS_8_PRO = "windows-8-pro"                         # Windows 8 Pro
-    WINDOWS_8_PRO_MC = "windows-8-pro-mc"                   # Windows 8 Pro with Media Center
-    WINDOWS_8_ENTERPRISE = "windows-8-enterprise"           # Windows 8 Enterprise
+    WINDOWS_7_STARTER = enum.auto()            # Windows 7 Starter
+    WINDOWS_7_HOME_BASIC = enum.auto()         # Windows 7 Home Basic
+    WINDOWS_7_HOME_PREMIUM = enum.auto()       # Windows 7 Home Premium
+    WINDOWS_7_PROFESSIONAL = enum.auto()       # Windows 7 Professional
+    WINDOWS_7_ULTIMATE = enum.auto()           # Windows 7 Ultimate
+    WINDOWS_7_ENTERPRISE = enum.auto()         # Windows 7 Enterprise
 
     # from https://www.windowsafg.com
-    WINDOWS_8_1 = "windows-8-1"                             # Windows 8.1
-    WINDOWS_8_1_PRO = "windows-8-1-pro"                     # Windows 8.1 Pro
-    WINDOWS_8_1_PRO_MC = "windows-8-1-pro-mc"               # Windows 8.1 Pro with Media Center
-    WINDOWS_8_1_ENTERPRISE = "windows-8-1-enterprise"       # Windows 8.1 Enterprise
+    WINDOWS_8 = enum.auto()                    # Windows 8
+    WINDOWS_8_PRO = enum.auto()                # Windows 8 Pro
+    WINDOWS_8_PRO_MC = enum.auto()             # Windows 8 Pro with Media Center
+    WINDOWS_8_ENTERPRISE = enum.auto()         # Windows 8 Enterprise
 
     # from https://www.windowsafg.com
-    WINDOWS_10_HOME = "windows-10-home"                         # Windows 10 Home
-    WINDOWS_10_PRO = "windows-10-pro"                           # Windows 10 Pro
-    WINDOWS_10_EDUCATION = "windows-10-education"               # Windows 10 Education
-    WINDOWS_10_ENTERPRISE = "windows-10-enterprise"             # Windows 10 Enterprise
-    WINDOWS_10_ENTERPRISE_LTSB = "windows-10-enterprise-ltsb"   # Windows 10 Enterprise LTSB
-
-    WINDOWS_11 = "windows-11"
+    WINDOWS_8_1 = enum.auto()                  # Windows 8.1
+    WINDOWS_8_1_PRO = enum.auto()              # Windows 8.1 Pro
+    WINDOWS_8_1_PRO_MC = enum.auto()           # Windows 8.1 Pro with Media Center
+    WINDOWS_8_1_ENTERPRISE = enum.auto()       # Windows 8.1 Enterprise
 
     # from https://www.windowsafg.com
-    WINDOWS_SERVER_2008 = "windows-server-2008"
-    WINDOWS_SERVER_2012 = "windows-server-2012"
-    WINDOWS_SERVER_2016 = "windows-server-2016"
-    WINDOWS_SERVER_2019 = "windows-server-2019"
+    WINDOWS_10_HOME = enum.auto()              # Windows 10 Home
+    WINDOWS_10_PRO = enum.auto()               # Windows 10 Pro
+    WINDOWS_10_EDUCATION = enum.auto()         # Windows 10 Education
+    WINDOWS_10_ENTERPRISE = enum.auto()        # Windows 10 Enterprise
+    WINDOWS_10_ENTERPRISE_LTSB = enum.auto()   # Windows 10 Enterprise LTSB
+
+    WINDOWS_11 = enum.auto()
+
+    # from https://www.windowsafg.com
+    WINDOWS_SERVER_2008 = enum.auto()
+    WINDOWS_SERVER_2012 = enum.auto()
+    WINDOWS_SERVER_2016 = enum.auto()
+    WINDOWS_SERVER_2019 = enum.auto()
 
 
 class Lang(enum.Enum):
-    en_US = "en_us"
-    zh_CN = "zh_cn"
-    zh_TW = "zh_tw"
+    en_US = enum.auto()
+    zh_CN = enum.auto()
+    zh_TW = enum.auto()
 
 
 class BootMode(enmu.Enum):
