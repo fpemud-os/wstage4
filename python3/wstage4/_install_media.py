430f8d1ab0dac6a67701f18ea295ab2ed0a81668
fpemud <fpemud@163.com>
2022-02-17 03:41:45 +0000

    fix

--- python3/wstage4/_install_media.py
+++ python3/wstage4/_install_media.py
@@ -95,10 +95,104 @@ class InstallMediaCustomizer:
 
     def __init__(self, tmp_dir, install_iso_file, target_iso_file):
         self._tmpdir = tmp_dir
+        self._target = target_iso_file
+        self._newFiles = dict()
 
+        # src
+        self._src = pycdlib.PyCdlib()
+        self._src.open(install_iso_file)
+        if iso.has_udf():
+            self._pathname = 'udf_path'
+        elif iso.has_rock_ridge():
+            self._pathname = 'rr_path'
+        elif iso.has_joliet():
+            self._pathname = 'joliet_path'
+        else:
+            self._pathname = 'iso_path'
+
+        # dst
+        self._dst = pycdlib.PyCdlib()
+        self._dst.new(interchange_level=args.iso_level,
+                      sys_ident=args.sysid,
+                      vol_ident=args.volid,
+                      set_size=args.volset_size,
+                      seqnum=args.volset_seqno,
+                      vol_set_ident=args.volset,
+                      pub_ident_str=args.publisher,
+                      preparer_ident_str=args.preparer,
+                      app_ident_str=args.appid,
+                      copyright_file=args.copyright,
+                      abstract_file=args.abstract,
+                      bibli_file=args.biblio,
+                      joliet=joliet_level,
+                      rock_ridge=rock_version,
+                      xa=(args.XA or args.xa),
+                      udf=udf_version
+        )
+
+    def add_dir(dir_path):
+        assert dir_path.startswith("/")
+        assert file_path not in self._newFiles
+
+        self._newFiles[file_path] = None
 
     def add_file(file_path, file_content):
-        pass
+        assert file_path.startswith("/")
+        assert file_path not in self._newFiles
 
-    def export(self):
-        pass
+        self._newFiles[file_path] = file_content
+
+    def update_file(file_path, file_content):
+        assert file_path.startswith("/")
+        assert file_path not in self._newFiles
+
+        self._newFiles[file_path] = file_content
+
+    def add_or_update_file(file_path, file_content):
+        assert file_path.startswith("/")
+
+        self._newFiles[file_path] = file_content
+
+    def export_and_dispose(self):
+        # export
+        root_entry = iso.get_record(**{pathname: "/"})
+        dirs = collections.deque([root_entry])
+        while dirs:
+            dir_record = dirs.popleft()
+            ident_to_here = iso.full_path_from_dirrecord(dir_record, rockridge=(pathname=='rr_path'))
+            relname = ident_to_here[len("/"):]
+            if relname and relname[0] == '/':
+                relname = relname[1:]
+            if dir_record.is_dir():
+                if relname != '':
+                    os.makedirs(os.path.join(self._tmpDir, relname))
+                child_lister = iso.list_children(**{pathname: ident_to_here})
+
+                for child in child_lister:
+                    if child is None or child.is_dot() or child.is_dotdot():
+                        continue
+                    dirs.append(child)
+            else:
+                if dir_record.is_symlink():
+                    fullpath = os.path.join(self._tmpDir, relname)
+                    local_dir = os.path.dirname(fullpath)
+                    local_link_name = os.path.basename(fullpath)
+                    old_dir = os.getcwd()
+                    os.chdir(local_dir)
+                    os.symlink(dir_record.rock_ridge.symlink_path(), local_link_name)
+                    os.chdir(old_dir)
+                else:
+                    iso.get_file_from_iso(os.path.join(self._tmpDir, relname), **{pathname: ident_to_here})
+
+        # dispose
+        self._dst.write(self._target)
+        self._dst.close()
+        self._dst = None
+
+        self._src.close()
+        self._pathname = None
+        self._src = None
+
+        self._newFiles = None
+        self._target = None
+        self._tmpdir = None
